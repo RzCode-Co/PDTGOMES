784a0536e658bb612225acdc512df81bd7422ad3
Anthony17DEV <109322252+Anthony17DEV@users.noreply.github.com>
2023-08-25 15:02:58 -0300

    Update Inicio.php

--- Inicio.php
+++ Inicio.php
@@ -6,3 +6,20 @@ $nomeUsuario = $_SESSION['nome'];
 $cargoUsuario = $_SESSION['cargo'];
 $fotoUsuario = $_SESSION['foto']; // Caminho da imagem
 ?>
+
+<?php
+// Puxa os parametros da pesquisa
+$termoPesquisa = $_GET['termo'];
+$filtroPesquisa = $_GET['filtro'];
+
+// Crie uma consulta base
+$consulta = "SELECT * FROM tabela WHERE 1"; // WHERE 1 é usado para garantir que todos os registros sejam considerados inicialmente
+
+// Adicione as condições de filtro de acordo com a escolha do usuário
+if (!empty($termoPesquisa)) {
+    $consulta .= " AND $filtroPesquisa LIKE '%$termoPesquisa%'";
+}
+
+// Exibe a consulta do bancario de dados
+// ...
+?>
