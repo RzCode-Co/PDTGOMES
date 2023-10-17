90e7368bb8151de4592e356801fecc2bf300d0d7
Caio Anderson <110497351+caioandersonf@users.noreply.github.com>
2023-10-17 00:03:59 -0300

    Add files via upload

--- pdt/Financeiro.php
+++ pdt/Financeiro.php
@@ -60,6 +60,116 @@ if ($result_meses_disponiveis->num_rows > 0) {
         }
     }
 }
+// Consulta SQL para calcular os valores de total_ganho, total_gasto e total_lucro (conforme mostrado no exemplo anterior)
+$sql_calcula_valores = "SELECT 
+    SUM(valor_venda) AS soma_valor_venda,
+    SUM(valor_servico) AS soma_valor_servico,
+    SUM(preco_total_geral) AS soma_preco_total_geral,
+    SUM(valor_debito) AS soma_valor_debito
+FROM valores";
+
+$result_calcula_valores = $conn->query($sql_calcula_valores);
+
+if ($result_calcula_valores) {
+    if ($result_calcula_valores->num_rows > 0) {
+        $row = $result_calcula_valores->fetch_assoc();
+
+        // Soma dos valores
+        $total_ganho = $row["soma_valor_venda"] + $row["soma_valor_servico"] + $row["soma_preco_total_geral"];
+        $total_gasto = $row["soma_valor_debito"];
+
+        // Cálculo do lucro
+        $total_lucro = $total_ganho - $total_gasto;
+
+        // Consulta SQL para inserir ou atualizar os valores na tabela saldos
+        $sql_insere_atualiza_saldos = "INSERT INTO saldos (id, total_ganho, total_lucro, total_gasto) 
+            VALUES (1, '$total_ganho', '$total_lucro', '$total_gasto') 
+            ON DUPLICATE KEY UPDATE 
+            total_ganho = VALUES(total_ganho), 
+            total_lucro = VALUES(total_lucro), 
+            total_gasto = VALUES(total_gasto)";
+    } else {
+        echo "Nenhum resultado encontrado na consulta para calcular valores.";
+    }
+} else {
+    echo "Erro na consulta SQL para calcular valores: " . $conn->error;
+}
+
+// Consulta SQL para obter os valores da tabela saldos
+$sql_saldos = "SELECT * FROM saldos WHERE id = 1";
+$result_saldos = $conn->query($sql_saldos);
+
+// Inicializar um array para armazenar os valores
+$valores_saldos = array();
+
+if ($result_saldos) {
+    if ($result_saldos->num_rows > 0) {
+        $saldos_row = $result_saldos->fetch_assoc();
+        $valores_saldos['total_ganho'] = $saldos_row['total_ganho'];
+        $valores_saldos['total_gasto'] = $saldos_row['total_gasto'];
+        $valores_saldos['total_lucro'] = $saldos_row['total_lucro'];
+    }
+}
+// Inicialize as variáveis $totalVenda e $totalDebito com 0
+$preco_total_geral = 0;
+$totalDebito = 0;
+
+// Função para obter o nome do mês em português
+function obterNomeMes($ano, $mes) {
+    setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+    return strftime('%B', strtotime("{$ano}-{$mes}-01"));
+}
+
+// Obter a data atual
+$dataAtual = date("Y-m-d");
+// Definir o fuso horário para evitar problemas de diferenças de data
+date_default_timezone_set('America/Sao_Paulo'); // Substitua 'America/Sao_Paulo' pelo fuso horário desejado
+// Calcular a data do domingo da semana atual
+$dataInicioSemana = date('Y-m-d', strtotime("last Sunday", strtotime($dataAtual)));
+// Calcular a data do sábado da semana atual
+$dataFimSemana = date('Y-m-d', strtotime("next Saturday", strtotime($dataInicioSemana)));
+$totalValoresGastos = 0;
+$dataSelecionada = "";
+$vendas = array();
+
+// Define a consulta SQL com um valor padrão
+$sql = "";
+
+// Verifica se o formulário foi submetido via POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $intervalo = $_POST["saldos"];
+
+    // Defina consultas SQL diferentes com base no intervalo selecionado.
+    if ($intervalo === "Dias") {
+        $dataConsulta = $_POST['intervalo-saldos'];
+        $sql = "SELECT * FROM valores WHERE DATE(data_venda) = '$dataConsulta'";
+        $dataSelecionada = $dataConsulta;
+    } elseif ($intervalo === "Semana") {
+        $sql = "SELECT * FROM valores WHERE DATE(data_venda) BETWEEN '$dataInicioSemana' AND '$dataFimSemana'";
+    } elseif ($intervalo === "Mes") {
+        $mesSelecionado = $_POST['mes'];
+        $sql = "SELECT * FROM valores WHERE MONTH(data_venda) = '$mesSelecionado'";
+        $dataSelecionada = obterNomeMes(date("Y"), $mesSelecionado);
+    } elseif ($intervalo === "Ano") {
+        $anoSelecionado = $_POST['ano'];
+        $sql = "SELECT * FROM valores WHERE YEAR(data_venda) = '$anoSelecionado'";
+        $dataSelecionada = $anoSelecionado;
+    }
+    
+    // Execute a consulta SQL correspondente e obtenha os resultados
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Processar e armazenar os resultados no array $historico
+        while ($row = $result->fetch_assoc()) {
+            $vendas[] = $row;
+
+            // Calcule os totais de vendas e débitos
+            $preco_total_geral += $row["preco_total_geral"];
+            $totalDebito += $row["valor_debito"];
+        }
+    }
+}
 $conn->close();
 ?>
 
@@ -68,6 +178,7 @@ $conn->close();
     <head>
         <meta charset="UTF-8">
         <title>Financeiro</title>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     </head>
     <style>
         body {
@@ -123,6 +234,17 @@ $conn->close();
         }
     </style>
     <body>
+        <div id="cabecalho">
+            <div id="usuario-info">
+                <img src="<?php echo $fotoUsuario; ?>" alt="Foto do Usuário">
+                <p><?php echo $nomeUsuario; ?></p>
+                <p><?php echo $cargoUsuario; ?></p>
+            </div>
+            <!-- Ícone de notificações -->
+            <div id="icone-notificacoes">
+                <img src="caminho-para-o-icone.png" alt="Ícone de Notificações">
+            </div>
+        </div>
         <!-- Seu menu lateral -->
         <div id="menu-lateral">
             <ul>
@@ -185,8 +307,9 @@ $conn->close();
 
         <button onclick="mostrarHistorico()">Mostrar Histórico</button>
         <button onclick="mostrarContas()">Mostrar Contas a Receber</button>
-        <a href="Processar_saldos.php" class="botao-redirecionamento">Mostrar Saldos e Débitos</a>
-        <form method="post" action="Consulta_saldos.php">
+        <button onclick="mostrarSaldos()">Mostrar Saldos e Débitos</button>
+        <button onclick="mostrarGastos()">Mostrar gastos</button>
+        <form method="post" action="Financeiro.php">
             <label>Consultar Saldos e Débitos por:
                 <select name="saldos" id="saldos" onchange="mostrarIntervaloDeTempo()">
                     <option value="" selected disabled>Escolha uma opção</option>
@@ -236,16 +359,159 @@ $conn->close();
             </div>
             <input type="submit" value="Consultar">
         </form>
+                <!-- Tabela para mostrar os valores -->
+        <div id="valores" style="display: none;">
+            <h1>Valores Financeiros</h1>
+            <table>
+                <tr>
+                    <th>Total Ganho</th>
+                    <th>Total Gasto</th>
+                    <th>Total Lucro</th>
+                </tr>
+                <tr>
+                    <td><?php echo $valores_saldos['total_ganho']; ?></td>
+                    <td><?php echo $valores_saldos['total_gasto']; ?></td>
+                    <td><?php echo $valores_saldos['total_lucro']; ?></td>
+                </tr>
+            </table>
+        </div>
+        <div id="tabelaGastos" style="display: none;">
+        <table>
+            <tr>
+                <th>Índice</th>
+                <th>Valor da Venda</th>
+                <th>Valor do Serviço</th>
+                <th>Preço Total Geral</th>
+                <th>Valor do Débito</th>
+                <th>Data da Venda</th>
+            </tr>
+            <?php
+            $indice = 1;
+            foreach ($vendas as $venda) {
+                echo "<tr>";
+                echo "<td>" . $indice . "</td>";
+                echo "<td>" . $venda["valor_venda"] . "</td>";
+                echo "<td>" . $venda["valor_servico"] . "</td>";
+                echo "<td>" . $venda["preco_total_geral"] . "</td>";
+                echo "<td>" . $venda["valor_debito"] . "</td>";
+                echo "<td>" . $venda["data_venda"] . "</td>";
+                echo "</tr>";
+                $indice++;
+            }
+            ?>
+        </table>
+    </div>
+        <div id="grafico-saldos" style="display: none;">
+            <canvas id="grafico" width="200" height="60"></canvas>
+                <script>
+                    // Acesse o contexto do canvas
+                    var ctx = document.getElementById("grafico").getContext("2d");
+
+                    // Variáveis do PHP para o gráfico
+                    var totalGanho = <?php echo $total_ganho; ?>;
+                    var totalGasto = <?php echo $total_gasto; ?>;
+                    var totalLucro = <?php echo $total_lucro; ?>;
+
+                    // Crie um gráfico em barra
+                    var grafico = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: ["Total Ganho", "Total Gasto", "Total Lucro"],
+                            datasets: [{
+                                label: 'Valores',
+                                data: [totalGanho, totalGasto, totalLucro],
+                                backgroundColor: [
+                                    'rgba(75, 192, 192, 0.6)',
+                                    'rgba(255, 99, 132, 0.6)',
+                                    'rgba(54, 162, 235, 0.6)',
+                                ],
+                                borderColor: [
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                ],
+                                borderWidth: 2
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    });
+                </script>
+        </div>
+        <div id="grafico-vendas" style="display: block;">
+            <canvas id="graficoVendas" width="400" height="200"></canvas>
+                <script>
+                    var ctx = document.getElementById('graficoVendas').getContext('2d');
+
+                    // Use os totais calculados no PHP para criar o gráfico
+                    var preco_total_geral = <?php echo $preco_total_geral ?? 0; ?>; // Defina um valor padrão se a variável não estiver definida
+                    var totalDebito = <?php echo $totalDebito ?? 0; ?>; // Defina um valor padrão se a variável não estiver definida
+                    var lucro = preco_total_geral - totalDebito;
+
+                    var data = {
+                        labels: ['Ganhos', 'Gastos', 'Lucro'],
+                        datasets: [{
+                            data: [preco_total_geral, totalDebito, lucro],
+                            backgroundColor: ['rgba(75, 192, 192, 0.6)', 'rgba(255, 99, 132, 0.6)', 'rgba(54, 162, 235, 0.6)'],
+                            borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
+                            borderWidth: 1
+                        }]
+                    };
+
+                    var options = {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    };
+
+                    var myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: data,
+                        options: options
+                    });
+                </script>
+            </div>
     </body>
     <script>
+        function mostrarGastos() {
+            document.getElementById("historico-de-vendas").style.display = "none";
+            document.getElementById("contas-a-receber").style.display = "none";
+            document.getElementById("tabelaGastos").style.display = "block";
+            document.getElementById("grafico-saldos").style.display = "none";
+            document.getElementById("grafico-vendas").style.display = "block";
+            document.getElementById("valores").style.display = "none";
+        }
+        function mostrarSaldos() {
+            document.getElementById("historico-de-vendas").style.display = "none";
+            document.getElementById("contas-a-receber").style.display = "none";
+            document.getElementById("tabelaGastos").style.display = "none";
+            document.getElementById("grafico-saldos").style.display = "block";
+            document.getElementById("grafico-vendas").style.display = "none";
+            document.getElementById("valores").style.display = "block";
+        }
         function mostrarHistorico() {
             document.getElementById("historico-de-vendas").style.display = "block";
             document.getElementById("contas-a-receber").style.display = "none";
+            document.getElementById("tabelaGastos").style.display = "none";
+            document.getElementById("grafico-saldos").style.display = "none";
+            document.getElementById("grafico-vendas").style.display = "none";
+            document.getElementById("valores").style.display = "none";
         }
 
         function mostrarContas() {
             document.getElementById("historico-de-vendas").style.display = "none";
             document.getElementById("contas-a-receber").style.display = "block";
+            document.getElementById("tabelaGastos").style.display = "none";
+            document.getElementById("grafico-saldos").style.display = "none";
+            document.getElementById("grafico-vendas").style.display = "none";
+            document.getElementById("valores").style.display = "none";
         }
         function mostrarIntervaloDeTempo() {
             var selectSaldos = document.getElementById("saldos");
