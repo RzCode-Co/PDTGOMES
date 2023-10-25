50b5c23c0bb9a342f86786348e05d1acc84b9e41
ARTHURGABRIEL22 <111550249+ARTHURGABRIEL22@users.noreply.github.com>
2023-10-25 00:07:35 -0300

    Add files via upload

--- SITE_FINAL/PHP/financeiro_historico.php
+++ SITE_FINAL/PHP/financeiro_historico.php
@@ -18,99 +18,139 @@ $conn->close();
 
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <title>Financeiro</title>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    </head>
-    <style>
-        body {
-            background-color: gray; /* Define o fundo cinza */
-            color: yellow; /* Define a cor do texto como amarelo */
-        }
-        
-        #conteudo {
-            margin: 20px; /* Adiciona margem para separar o conteúdo do cabeçalho e do menu lateral */
-        }
-        
-        /* Estilização básica para o cabeçalho */
-        #cabecalho {
-            background-color: black; /* Cor de fundo do cabeçalho (pode ajustar conforme desejado) */
-            color: white; /* Cor do texto no cabeçalho (pode ajustar conforme desejado) */
-            padding: 10px; /* Espaçamento interno no cabeçalho */
-            display: flex; /* Para alinhar os elementos do cabeçalho na horizontal */
-            justify-content: space-between; /* Distribui os elementos horizontalmente */
-        }
-        
-        #usuario-info {
-            display: flex; /* Alinha os elementos do usuário na horizontal */
-            align-items: center; /* Centraliza verticalmente os elementos do usuário */
-        }
-        
-        #icone-notificacoes {
-            /* Adicione estilos para o ícone de notificações, como tamanho, margem, etc. */
-        }
-        
-        /* Estilização para o menu lateral */
-        #menu-lateral {
-            background-color: black; /* Cor de fundo do menu (pode ajustar conforme desejado) */
-        }
-        
-        #menu-lateral ul {
-            list-style-type: none; /* Remove marcadores de lista */
-            padding: 0; /* Remove o preenchimento padrão da lista */
-        }
-        
-        #menu-lateral ul li {
-            margin: 0; /* Remove a margem padrão dos itens da lista */
-        }
-        
-        #menu-lateral ul li a {
-            display: block; /* Transforma os links em blocos para preencher o espaço disponível */
-            padding: 10px 20px; /* Espaçamento interno nos links */
-            color: white; /* Cor do texto dos links */
-            text-decoration: none; /* Remove sublinhado dos links */
-        }
-        
-        #menu-lateral ul li a:hover {
-            background-color: gray; /* Cor de fundo quando o mouse passa por cima */
-        }
+
+<head>
+    <meta charset="UTF-8">
+    <title>Financeiro</title>
+    <link rel="stylesheet" href="../CSS/financeiro.css">
+    <link rel="stylesheet" href="../CSS/pagina_inicial.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+
 </style>
-    </style>
-    <body>
-        <div id="cabecalho">
-            <div id="usuario-info">
-                <img src="<?php echo $fotoUsuario; ?>" alt="Foto do Usuário">
-                <p><?php echo $nomeUsuario; ?></p>
-                <p><?php echo $cargoUsuario; ?></p>
-            </div>
-            <!-- Ícone de notificações -->
-            <div id="icone-notificacoes">
-                <img src="caminho-para-o-icone.png" alt="Ícone de Notificações">
-            </div>
-        </div>
-        <!-- Seu menu lateral -->
-        <div id="menu-lateral">
-            <ul>
-                <li><a href="../PHP/inicio.php">Inicio</a></li>
-                <li><a href="Venda.html">Venda</a></li>
-                <li><a href="Financeiro.html">Financeiro</a></li>
-                <li><a href="../PHP/Debitos.php">Debitos</a></li>
-                <li><a href="../PHP/Notificações.php">Notificações</a></li>
-                <li><a href="../PHP/Estoque.php">Estoque</a></li>
-                <li><a href="../PHP/Criação OS.php">Criação/Consulta de OS</a></li>
-            </ul>
+
+<body>
+
+    <nav class="menu_lateral">
+
+        <!-- Barra MENU -->
+        <div class="btn_expandir">
+            <img src="../CSS/img/Três barras.svg" alt="menu" id="btn_exp">
         </div>
 
-        <div id="historico-de-vendas">
-            <h1>Historico de vendas</h1>
+        <!--  itens MENU LATERAL-->
+        <ul class="ul_menu_lateral">
+
+            <li class="item_menu ativo">
+                <a href="../HTML/pagina_incial.html">
+                    <img class="icon" src="../CSS/img/Logo Circular verde.svg" alt="logo">
+                    <span class="txt_link">Home</span>
+                </a>
+            </li>
+
+            <li class="item_menu">
+                <a href="../HTML/Venda.html">
+                    <img class="icon" src="../CSS/img/VENDAS.svg" alt="icone compras">
+                    <span class="txt_link">Vendas</span>
+                </a>
+            </li>
+
+            <li class="item_menu">
+                <a href="../PHP/estoque.php">
+                    <img class="icon" src="../CSS/img/Compras.svg" alt="icone compras">
+                    <span class="txt_link">Estoque</span>
+                </a>
+            </li>
+
+
+            <li class="item_menu">
+                <a href="../PHP/Graficos.php">
+                    <img class="icon" src="../CSS/img/Gráficos.svg" alt="icone graficos">
+                    <span class="txt_link">Gráficos</span>
+                </a>
+            </li>
+
+            <li class="item_menu">
+                <a href="../HTMl/Financeiro.html">
+                    <img class="icon" src="../CSS/img/Carteira.svg" alt="icone carteira">
+                    <span class="txt_link">Históricos</span>
+                </a>
+            </li>
+
+            <li class="item_menu">
+                <a href="../PHP/Criação OS.php">
+                    <img class="icon" src="../CSS/img/OS.svg" alt="icone OS">
+                    <span class="txt_link">O.S</span>
+                </a>
+            </li>
+
+            <li class="item_menu">
+                <a href="#">
+                    <img class="icon" src="../CSS/img/Perfil.svg" alt="icone perfil">
+                    <span class="txt_link">Perfil</span>
+                </a>
+            </li>
+
+            <li class="item_menu">
+                <a href="../PHP/Notificações.php">
+                    <img class="icon" src="../CSS/img/Sino.svg" alt="logo">
+                    <span class="txt_link">Notificações</span>
+                </a>
+            </li>
+
+        </ul>
+        <!-- importando o JS para o Menu Lateral-->
+        <script src="../JS/menu.js"></script>
+
+    </nav>
+
+    <!-- Menu horizonatl -->
+    <nav class="menu_horizontal">
+        <ul>
+            <li id="logo_menu_horizontal"><a href="../HTML/pagina_incial.html"><img src="../CSS/img/Logo Horizontal.png"
+                        alt="logo da empresa"></a>
+            </li>
+
+            <li id="direita">
+
+                <!-- Perfil -->
+                <div class="image_container">
+                    <img src="../CSS/img/editar.png" alt="insira foto de perfil" id="img_photo">
+                </div>
+
+                <!-- Escolhendo Imagem -->
+                <input type="file" id="file_image" name="file_image" accept="image/*">
+
+                <script src="../JS/login_preview.js"></script>
+            </li>
+
+            <li id="direita">
+                <!-- Cargo e nome -->
+                <div class="cargo_nome">
+                    <h3>Cargo</h3>
+                    <p>Nome e Sobrenome</p>
+                </div>
+            </li>
+
+            <li id="direita"><a href="#"><img src="../CSS/img/Sino_menu_horizontal.svg" alt="Notificações"></a></li>
+
+        </ul>
+
+    </nav>
+
+    <div id="historico_vendas">
+        <div class="titulo_icone">
+            <a id="icone_voltar" href="../HTML/financeiro.html"><img src="../CSS/img/voltar.svg" alt="voltar página"></a>
+            <h1>Histórico de Vendas</h1>
+        </div>
+        <div class="centralização">
             <table>
-            <?php
+                <?php
                 // Defina o número de itens por página
                 $itemsPerPage = 10;
 
                 // Obtenha a página atual a partir dos parâmetros da URL
-                $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+                $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
 
                 // Calcule o índice de início e fim para os itens da página atual
                 $startIndex = ($page - 1) * $itemsPerPage;
@@ -128,7 +168,7 @@ $conn->close();
                     </tr>';
 
                 $displayedCount = 0; // Contador para controlar a exibição dos itens
-
+                
                 foreach ($historico as $venda) {
                     if ($displayedCount >= $startIndex && $displayedCount < $endIndex) {
                         echo '<tr>';
@@ -145,13 +185,15 @@ $conn->close();
 
                 echo '</table>';
 
+                echo '</div>';
+
                 // Adicione os links de paginação
                 echo '<div id="pagination">';
                 $totalItems = count($historico);
                 $totalPages = ceil($totalItems / $itemsPerPage);
 
                 $displayedPages = 5; // Número de páginas a serem exibidas na páginação
-
+                
                 if ($totalPages > 1) {
                     $currentPage = $page;
                     $firstPage = max(1, $currentPage - floor($displayedPages / 2));
@@ -159,7 +201,7 @@ $conn->close();
 
                     for ($i = $firstPage; $i <= $lastPage; $i++) {
                         echo '<a href="financeiro_historico.php?page=' . $i . '">' . $i . '</a> ';
-                        
+
                     }
 
                     if ($lastPage < $totalPages) {
@@ -172,8 +214,9 @@ $conn->close();
                 }
 
                 echo '</div>';
-            ?>
+                ?>
             </table>
         </div>
-    </body>
+</body>
+
 </html>
