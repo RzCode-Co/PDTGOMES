ea34129da5114e677530999c537995b64ec839d2
Caio Anderson <110497351+caioandersonf@users.noreply.github.com>
2023-10-27 00:06:01 -0300

    Add files via upload

--- SITE_FINAL/PHP/processar_cadastro_adm.php
+++ SITE_FINAL/PHP/processar_cadastro_adm.php
@@ -2,11 +2,11 @@
     require_once "config.php"; // Arquivo de configuração do banco de dados
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Verifica se os campos obrigatórios estão preenchidos
-        if (isset($_POST['nome'], $_POST['senha'])) {
+        if (isset($_POST['nome'], $_POST['senha'], $_POST['cargo'])) {
             // Validação e sanitização dos campos de entrada
             $nome = filter_var($_POST['nome'], FILTER_SANITIZE_STRING);
             $senha = filter_var($_POST['senha'], FILTER_SANITIZE_STRING);
-            $cargo = "cliente";
+            $cargo = filter_var($_POST['cargo'], FILTER_SANITIZE_STRING);
             $cpf_cnpj = filter_var($_POST['cpf_cnpj'], FILTER_SANITIZE_STRING);
             // Verifique se a escolha do usuário (CPF ou CNPJ) é válida
             if ($_POST["cpf_cnpj"] == "CPF") {
@@ -23,9 +23,22 @@
                 </script>';
                 exit; // Saia do script
             }
-            $diretorioDestino = "../img_clientes/"; // Diretório onde os arquivos serão armazenados
-            $nomeArquivo = $_FILES["arquivo"]["name"];
-            $caminhoCompleto = $diretorioDestino . $nomeArquivo;
+            if ($_POST["cargo"] == "cliente") {
+                // Configurações para o arquivo
+                $diretorioDestino = "../img_clientes/"; // Diretório onde os arquivos serão armazenados
+                $nomeArquivo = $_FILES["arquivo"]["name"];
+                $caminhoCompleto = $diretorioDestino . $nomeArquivo;
+            } elseif ($_POST["cargo"] == "vendedor") {
+                // Configurações para o arquivo
+                $diretorioDestino = "../img_vendedores/"; // Diretório onde os arquivos serão armazenados
+                $nomeArquivo = $_FILES["arquivo"]["name"];
+                $caminhoCompleto = $diretorioDestino . $nomeArquivo;
+            } elseif ($_POST["cargo"] == "admin") {
+                // Configurações para o arquivo
+                $diretorioDestino = "../img_adm/"; // Diretório onde os arquivos serão armazenados
+                $nomeArquivo = $_FILES["arquivo"]["name"];
+                $caminhoCompleto = $diretorioDestino . $nomeArquivo;
+            }
             // Instrução SQL segura usando prepared statement
             $stmt = $conn->prepare("SELECT id FROM usuarios WHERE nome = ? AND senha = ? AND cargo = ? AND cpf_cnpj = ? AND CPF = ? AND CNPJ = ?");
             $stmt->bind_param("ssssss", $nome, $senha, $cargo, $cpf_cnpj, $CPF, $CNPJ);
