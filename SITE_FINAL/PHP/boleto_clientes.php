46da4d27f6e753b3e4c24841493826283e5ea96c
ARTHURGABRIEL22 <111550249+ARTHURGABRIEL22@users.noreply.github.com>
2023-11-11 01:09:00 -0300

    Add files via upload

--- SITE_FINAL/PHP/boleto_clientes.php
+++ SITE_FINAL/PHP/boleto_clientes.php
@@ -1,66 +1,91 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <title>Boleto do Cliente</title>
+    <link rel="stylesheet" href="../CSS/nfe_clientes.css">
 </head>
+
 <body>
-    <h1>Boleto do Cliente</h1>
+    <div class="titulo_icone">
+        <a id="icone_voltar" href="../PHP/Debitos.php"><img src="../CSS/img/voltar.svg" alt="voltar página"></a>
+        <h1>NFE CLIENTES</h1>
+    </div>
     <?php
-    // Conecte-se ao banco de dados ou inclua seu arquivo de configuração do banco de dados
     require_once "config.php";
 
-    $ano = date('Y');
-
-    // Consulta para obter os dados dos clientes
     $sql = "SELECT id, nome, cpf, cnpj FROM usuarios WHERE cargo = 'cliente'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $clienteId = $row['id'];
-            $clienteNome = $row['nome'];
-            $clienteCPF = $row['cpf'];
-            $clienteCNPJ = $row['cnpj'];
+        ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>CPF/CNPJ</th>
+                    <th>Ação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                while ($row = $result->fetch_assoc()) {
+                    $clienteId = $row['id'];
+                    $clienteNome = $row['nome'];
+                    $clienteCPF = $row['cpf'];
+                    $clienteCNPJ = $row['cnpj'];
 
-            echo "<p>Nome: $clienteNome</p>";
+                    echo "<tr>";
+                    echo "<td>$clienteNome</td>";
 
-            if ($clienteCPF !== '0') {
-                echo "<p>CPF: $clienteCPF</p>";
-            } elseif ($clienteCNPJ !== '0') {
-                echo "<p>CNPJ: $clienteCNPJ</p>";
-            }
-            // Adicione o campo hidden para armazenar o clienteId
-            echo "<form action='download_boleto_cliente.php' method='post'>";
-            echo "<input type='hidden' name='CPF' value='$clienteCPF'>";
-            echo "<input type='hidden' name='CNPJ' value='$clienteCNPJ'>";
-            echo "<label for='mes'>Selecione o mês:</label>";
-            echo "<select name='mes' id='mes'>";
-            echo "<option value='1'>Janeiro</option>";
-            echo "<option value='2'>Fevereiro</option>";
-            echo "<option value='3'>Março</option>";
-            echo "<option value='4'>Abril</option>";
-            echo "<option value='5'>Maio</option>";
-            echo "<option value='6'>Junho</option>";
-            echo "<option value='7'>Julho</option>";
-            echo "<option value='8'>Agosto</option>";
-            echo "<option value='9'>Setembro</option>";
-            echo "<option value='10'>Outubro</option>";
-            echo "<option value='11'>Novembro</option>";
-            echo "<option value='12'>Dezembro</option>";
-            echo "</select>";
-            echo"<input type='hidden' name='ano' value='$ano'>";
-            echo "<input type='submit' value='Gerar Boleto'>";
-            echo "</form>";
+                    if ($clienteCPF !== '0') {
+                        echo "<td>$clienteCPF</td>";
+                    } elseif ($clienteCNPJ !== '0') {
+                        echo "<td>$clienteCNPJ</td>";
+                    }
 
-            echo "<hr>";
-        }
+                    echo "<td>";
+                    echo "<form action='gerar_boleto.php' method='post'>";
+                    echo "<input type='hidden' name='clienteCPF' value='$clienteCPF'>";
+                    echo "<input type='hidden' name='clienteCNPJ' value='$clienteCNPJ'>";
+                    echo "<label for='mes'>Selecione o mês:</label>";
+                    echo "<select name='mes'>";
+                    $meses = [
+                        'Janeiro',
+                        'Fevereiro',
+                        'Março',
+                        'Abril',
+                        'Maio',
+                        'Junho',
+                        'Julho',
+                        'Agosto',
+                        'Setembro',
+                        'Outubro',
+                        'Novembro',
+                        'Dezembro'
+                    ];
+                    foreach ($meses as $key => $mes) {
+                        $numeroMes = $key + 1;
+                        echo "<option value='$numeroMes'>$mes</option>";
+                    }
+                    echo "</select>";
+                    echo "<input type='submit' value='Gerar NFE'>";
+                    echo "</form>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+
+        <?php
     } else {
-        echo "Nenhum cliente encontrado.";
+        echo "<p>Nenhum cliente encontrado.</p>";
     }
 
-    // Feche a conexão com o banco de dados (lembre-se de ajustar isso de acordo com sua configuração)
     $conn->close();
     ?>
 </body>
+
 </html>
