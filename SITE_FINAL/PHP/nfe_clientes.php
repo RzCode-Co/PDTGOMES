12c0065dd66e6a2bd7e903faa2e46cc7c694e03c
Anthony17DEV <109322252+Anthony17DEV@users.noreply.github.com>
2023-11-11 01:28:12 -0300

    Update nfe_clientes.php

--- SITE_FINAL/PHP/nfe_clientes.php
+++ SITE_FINAL/PHP/nfe_clientes.php
@@ -1,3 +1,35 @@
+<?php
+require_once "config.php";
+
+// Número de clientes por página
+$items_per_page = 10;
+
+$sql = "SELECT id, nome, cpf, cnpj FROM usuarios WHERE cargo = 'cliente'";
+$result = $conn->query($sql);
+
+// Total de resultados
+$total_results = $result->num_rows;
+
+// Número total de páginas
+$total_pages = ceil($total_results / $items_per_page);
+
+// Página atual
+if (isset($_GET['page']) && is_numeric($_GET['page'])) {
+    $current_page = intval($_GET['page']);
+} else {
+    $current_page = 1;
+}
+
+// Calcula o índice de início e fim dos resultados
+$index_start = ($current_page - 1) * $items_per_page;
+$index_end = $index_start + $items_per_page;
+
+// Reexecute a consulta com LIMIT para obter os resultados da página atual
+$sql .= " LIMIT $index_start, $items_per_page";
+$result = $conn->query($sql);
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -12,14 +44,11 @@
         <a id="icone_voltar" href="../PHP/Debitos.php"><img src="../CSS/img/voltar.svg" alt="voltar página"></a>
         <h1>NFE CLIENTES</h1>
     </div>
+
     <?php
-    require_once "config.php";
-
-    $sql = "SELECT id, nome, cpf, cnpj FROM usuarios WHERE cargo = 'cliente'";
-    $result = $conn->query($sql);
-
     if ($result->num_rows > 0) {
-        ?>
+    ?>
+
         <table>
             <thead>
                 <tr>
@@ -80,6 +109,21 @@
         </table>
 
         <?php
+        // Exibir links de páginação
+        echo "<div class='pagination'>";
+        if ($total_pages > 1) {
+            for ($i = 1; $i <= $total_pages; $i++) {
+                if ($i == $current_page) {
+                    echo "<span class='current'>$i</span>";
+                } else {
+                    echo "<a href='?page=$i'>$i</a>";
+                }
+            }
+        }
+        echo "</div>";
+        ?>
+
+    <?php
     } else {
         echo "<p>Nenhum cliente encontrado.</p>";
     }
